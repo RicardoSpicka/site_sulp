81ec734bd1a0646711c3be4bd4a49876f5ac32d3
Ricardo Spicka <ricco.spicka@gmail.com>
2023-07-30 20:00:15 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,124 @@
+@extends('layouts.main')
+
+@section('title', 'Sulplast')
+
+@section('content')
+
+<section class='header'>
+    <div>
+        <img src="/image/logotipo-sulplast.png" alt="logotipo-sulplast">
+    </div>
+
+        <ul>
+            <li><img src="./image/phone.png" alt="">Tel.: +55 (19) <strong>3535-6550</strong></li>
+            <li><img src="./image/email.png" alt="">E-mail: <strong>sulplast@sulplast.com.br</strong></li>
+            <li><img src="./image/locate.png" alt="">Av. Sulplast, 1969 - Distrito Industrial - Rio Claro/SP - Brasil - 13505-680</li>
+        </ul>
+</section>
+<navbar class="navBar">
+    <ul>
+        <li>A empresa</li>
+        <li>Certificações e prêmios</li>
+        <li>Tecnologias e processos</li>
+        <li>Meio ambiente</li>
+        <li>Canais de comunicação</li>
+        <li>Notícias</li>
+    </ul>
+</navbar>
+
+<slider>
+    <div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="2000">
+        <img src="./image/40anos.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+        <img src="./image/fibra.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+        <img src="./image/pintura.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+        <img src="./image/termo.png" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+</slider>
+<div class="cards">
+    <card class="info">
+        <div class="info-left">
+            <h1>Sulplast</h1>
+            <p>Há mais de 40 anos no mercado, a Sulplast é uma empresa especializada no desenvolvimento e fabricação de peças técnicas em termoplástico e fibra de vidro. Capaz de atender aos mais rigorosos padrões de qualidade e segurança, a Sulplast possui expertise nos processos de Vacuum Forming, Rotomoldagem, Injeção, RTM Light, RTM Skin, Hand Lay Up e Pintura Classe A.</p>
+            <div>
+                <button>Sobre a Sulplast</button>
+                <img src="./image/q1.png" alt="q1">
+                <img src="./image/selo-vda-63.png" alt="selo-vda-63">
+                <img src="./image/selo-iso9001.png" alt="selo-iso9001">
+                <img src="./image/selo-iso14001.png" alt="selo-iso14001">
+                <img src="./image/selo-iatf16949.png" alt="selo-iatf16949">
+            </div>
+        </div>
+        <div class="info-right">
+            <img src="./image/sulplast-circulo.png" alt="sulplast-circulo.png">
+        </div>
+    </card>
+    <card class="card">
+        <div class="side-text">
+            <h1>Automotivo</h1>
+            <p>Com forte atuação neste setor, especialmente no segmento de caminhões e veículos especiais, as peças automotivas <strong>Sulplast</strong> possuem elevado desempenho e atendem as exigências das principais montadoras do mundo.</p>
+        </div>
+        <div class="side-image">
+            <img src="./image/setor-automotivo-3.jpg" alt="setor-automotivo-3">
+        </div>
+    </card>
+    <card class="card">
+        <div class="side-image">
+            <img src="./image/setor-agricola.jpg" alt="setor-agricola">
+        </div>
+        <div class="side-text">
+            <h1>Agrícola</h1>
+            <p>Fornecedora de soluções plásticas para os principais fabricantes de máquinas e implementos agrícolas presentes no mercado, a <strong>Sulplast</strong> contribui para a expansão do agronegócio e aumento da produção de alimentos ao redor do planeta.</p>
+        </div>
+    </card>
+    <card class="card">
+        <div class="side-text">
+            <h1>Construção e Mineração</h1>
+            <p>A <strong>Sulplast</strong> também fornece componentes de alta qualidade para os principais fabricantes de máquinas e implementos para construção civil e mineração, contribuindo em obras de infraestrutura e na extração de recursos naturais.</p>
+        </div>
+        <div class="side-image">
+            <img src="./image/setor-civil.jpg" alt="setor-civil">
+        </div>
+    </card>
+    <card class="card">
+        <div class="side-image">
+            <img id="div-set-eco" src="./image/diversos-setores-da-economia.jpg" alt="diversos-setores-da-economia">
+        </div>
+        <div class="side-text">
+            <h1>Diversos Setores da Economia</h1>
+            <p>A estrutura e o conhecimento técnico da <strong>Sulplast</strong> tornam a empresa capaz de desenvolver soluções plásticas eficientes para todos os setores da economia, com a excelência que destaca nossos produtos e processos.</p>
+        </div>
+    </card>
+    <card class="card">
+        <div class="side-text">
+            <h1>Meio ambiente</h1>
+            <p>A <strong>Sulplast</strong> assume o compromisso de construir um mundo mais sustentável. Certificado pela norma <strong>ISO 14001</strong>, nosso <strong>Sistema de Gestão Ambiental</strong> é rigorosamente aplicado em todas as etapas produtivas da empresa.</p>
+        </div>
+        <div class="side-image premios-certif" style="background-image: url(./image/background-split-image.jpg); height: 330px; color: aliceblue;">
+            <ul class="premios-certif-list">
+                <li><img src="./image/certificate.png" alt="">Certificações</li>
+                <li><img src="./image/trofeus.png" alt="">Prêmios</li>
+            </ul>
+            <p>Sempre em busca do mais alto nível de qualidade para seus produtos e processos, a Sulplast conquistou e mantém renomadas certificações nacionais e internacionais.</p>
+
+            <a href="">Nossas certificações</a>
+        </div>
+    </card>
+</div>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="favicon.png">
+        <script src="https://kit.fontawesome.com/0362170bf2.js" crossorigin="anonymous"></script>
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+        <!-- Fontes do Google -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato" rel="stylesheet">
+
+        <!-- Link para CSS -->
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/bootstrap.js"></script>
+
+        <title>@yield('title')</title>
+
+    </head>
+    <body>
+
+        @yield('content')
+
+        <footer>
+            <div class="collumns">
+                <div class="collumn logos">
+                    <img id="sulp-logo" src="./image/logotipo-sulplast-rodape.png" alt="">
+                    <img id="duns" src="./image/duns.png" alt="">
+                </div>
+                <div class="collumn">
+                    <h3>Empresa</h3>
+                    <ul>
+                        <li>Quem somos</li>
+                        <li>Missão e visão</li>
+                        <li>Certificações e prêmios</li>
+                        <li>Política de ética empresarial e sustentabilidade</li>
+                        <li>Notícias</li>
+                    </ul>
+                </div>
+                <div class="collumn">
+                    <h3>Tecnologias e processos</h3>
+                    <ul>
+                        <li>Termoplástico</li>
+                        <li>Fibra de vidro</li>
+                        <li>Pintura</li>
+                        <li>Desenvolvimento de produtos</li>
+                    </ul>
+                </div>
+                <div class="collumn">
+                    <h3>Meio ambiente</h3>
+                    <ul>
+                        <li>A Sulplast e o meio ambiente</li>
+                        <li>Ciclo de vida do produto</li>
+                        <li>Fornecedores e meio ambiente</li>
+                    </ul>
+                </div>
+                <div class="collumn">
+                    <h3>Fale conosco</h3>
+                    <ul>
+                        <li>Fale conosco</li>
+                        <li>Localização</li>
+                        <li>Trabalhe conosco</li>
+                    </ul>
+                </div>
+            </div>
+            <p>
+            2023 - <strong>Sulplast</strong>, todos os direitos reservados. As imagens apresentadas neste website são meramente ilustrativas. <br><strong>Sulplast - Fibra de vidro e termoplástico LTDA</strong>. Avenida Sulplast, 1969 - Distrito Industrial - Rio Claro/SP - Brasil. <strong><br>Crédito das imagens</strong>. Desenvolvido por <strong>Ricardo Spicka</strong>.
+            </p>
+        </footer>
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+    </body>
+</html>
